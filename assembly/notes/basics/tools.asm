650b257a8d1c3bfce8bff2aea4cfc6b16e8bbd4e
JackOfBlades232 <petrkiyashko232@gmail.com>
2022-07-14 14:50:07 +0300

    updated tools.asm

--- assembly/notes/basics/tools.asm
+++ assembly/notes/basics/tools.asm
@@ -1,6 +1,5 @@
 %include "../../stud_io.inc"
 global _start
-;; first part of the code was lost, to be recovered
 
 section .data
 fibon   dw 1, 1, 2, 3, 5, 8, 13, 21 ; 8 2-byte words to array
@@ -20,7 +19,7 @@ matrix  resd 10*15      ; memory for matrix
 
 section .text
 fcircle dw 360          ; initialize const
-        mov eax, ebx    ; cp from ebx to eax
+_start: mov eax, ebx    ; cp from ebx to eax
         mov ecx, 5      ; put 5 in ecx
         mov [x], ecx    ; cp from ecx to mem in x
         mov edx, [x]    ; cp from mem in x to edx, cant move from mem to mem
@@ -33,4 +32,22 @@ fcircle dw 360          ; initialize const
         add eax, ebx    ; add ebx to eax and put in eax
         sub [x], ecx    ; subtract ecx from mem in x and put in mem in x
         add edx, 12     ; add 12 to edx and put in edx
+        add dword [x], 12   ; add 12 to mem at x, have to spec type
+        add eax, ecx    ; add lower parts
+        adc edx, ebx    ; add upper parts with overflow
+        sub eax, ecx    ; sub lower parts
+        sbb edx, ebx    ; sub upper parts with overflow
+        cmp ecx, 25     ; compare and update flag
+        mul bl          ; mult al by bl, res in ax
+        mul bx          ; mult ax by bx, res in dx:ax
+        mul ebx         ; mult eax by ebx, res in edx:eax
+        imul bx         ; same, but with sign
+        div bl          ; div ax by bl, q in al, r in ah
+        div bx          ; div dx:ax by bx, q in ax, r in dx
+        div ebx         ; same, with e-s
+        idiv bx         ; signed
+        cbw             ; convert al to ax (fill ah with sign of al)
+        cwd             ; convert ax to dx:ax
+        cwde            ; convert ax to eax
+        cdq             ; convert eax to edx:eax
         FINISH
