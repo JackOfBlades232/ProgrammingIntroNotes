735837f5acdbc8bab2e7a5bf95e5ef7100dd1e2c
JackOfBlades232 <petrkiyashko232@gmail.com>
2022-10-03 21:13:44 +0300

    Minor cleanup

--- pascal/problems/2_45/test.sh
+++ pascal/problems/2_45/test.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+
 fpc -dTEST rbtree.pp > /dev/null
 fpc -dTEST unitdemo.pas > /dev/null
 fpc -dTEST generate_test > /dev/null
+
 I=1
 while [ $I -le $1 ]; do
     ./generate_test > input.txt
     ./unitdemo < input.txt
     I=$(( I + 1 ))
 done
+
 rm input.txt
