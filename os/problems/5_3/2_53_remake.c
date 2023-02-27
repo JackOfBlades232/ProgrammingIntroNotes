0da45a73a079fee5e392213bd78869022665c6af
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-02-27 15:10:40 +0200

    5_3 WIP 2

--- os/problems/5_3/2_53_remake.c
+++ os/problems/5_3/2_53_remake.c
@@ -7,7 +7,12 @@
 
 int main(int argc, char **argv)
 {
-    int in_fd, spc_fd, cnt_fd;
+    FILE *in_f, *spc_f;
+    int cnt_fd;
+
+    int c;
+    int cur_line_ln;
+    int write_to_spc;
 
     if (argc < 4) {
         fprintf(stderr, "Specify input file name, file name for lines and "
@@ -15,31 +20,53 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    in_fd = open(argv[1], O_RDONLY);
-    if (in_fd == -1) {
+    in_f = fopen(argv[1], "r");
+    if (!in_f) {
         perror(argv[1]);
         return 2;
     }
 
-    spc_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    if (spc_fd == -1) {
+    spc_f = fopen(argv[2], "w");
+    if (!spc_f) {
         perror(argv[2]);
-        close(in_fd);
+        fclose(in_f);
         return 3;
     }
 
-    cnt_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    cnt_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (cnt_fd == -1) {
         perror(argv[3]);
-        close(in_fd);
-        close(spc_fd);
+        fclose(in_f);
+        fclose(spc_f);
         return 4;
     }
 
+    cur_line_ln = 0;
+    write_to_spc = 0;
+    while ((c = getc(in_f)) != EOF) {
+        if (c == '\n' || c == '\r') {
+            write(cnt_fd, &cur_line_ln, sizeof(cur_line_ln));
+            cur_line_ln = 0;
 
+            if (write_to_spc) {
+                putc(c, spc_f);
+                write_to_spc = 0;
+            }
 
-    close(in_fd);
-    close(spc_fd);
+            continue;
+        }
+
+        if (cur_line_ln == 0 && c == ' ')
+            write_to_spc = 1;
+
+        if (write_to_spc)
+            putc(c, spc_f);
+
+        cur_line_ln++;
+    }
+
+    fclose(in_f);
+    fclose(spc_f);
     close(cnt_fd);
     return 0;
 }
