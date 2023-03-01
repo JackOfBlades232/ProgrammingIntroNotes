9a8c13faf981dfdb1c8eabf930f654bc2b139684
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-03-01 07:29:39 +0200

    Tested times for 2_57_remake

--- os/problems/5_7/gen_rand_list.sh
+++ os/problems/5_7/gen_rand_list.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # 5_7/gen_rand_list.sh
 
-GENERATOR=$1
-LIST_NM=$2
-SIZE=$3
-
 I=0
-while [ $I -lt $SIZE ]; do
-    $GENERATOR $TABLE add $(echo $RANDOM | md5sum | head -c 20)
+while [ $I -lt $3 ]; do
+    $1 $2 add $(echo $(( RANDOM + I )) | md5sum | head -c 20)
+    I=$(( I + 1 ))
 done
