b88fba549b61d8dd8be8461a466a7571e5c43e9f
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-03-16 18:04:20 +0200

    Added typing and backspace handling to 5_34

--- os/problems/5_34-5_35/5_34.c
+++ os/problems/5_34-5_35/5_34.c
@@ -1,4 +1,5 @@
 /* 5_34.c */
+#include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
@@ -9,7 +10,8 @@ enum {
 };
 
 enum { 
-    io_bufsize = 512,
+    read_bufsize = 1024,
+    output_bufsize = 4096,
     match_bufsize = 128 
 };
 
@@ -69,7 +71,7 @@ int look_up_word_by_prefix(FILE *dict, const char *prefix, int prefix_len,
     return num_matches;
 }
 
-void complete_word(long dict_pos, FILE *dict_f, char **bufpos, char *buf)
+void complete_word(long dict_pos, FILE *dict_f)
 {
     fseek(dict_f, dict_pos, SEEK_SET);
     output_to_eol(dict_f);
@@ -86,11 +88,40 @@ void output_multiple_matches(long *matches, int match_cnt, FILE *dict_f)
     putchar_now('\n');
 }
 
+int add_char_to_buf(char **bufpos, char *buf, size_t bufsize, char c)
+{
+    if (*bufpos - buf >= bufsize-1)
+        return 0;
+
+    **bufpos = c;
+    (*bufpos)++;
+    **bufpos = '\0';
+    return 1;
+}
+
+int remove_char_from_buf(char **bufpos, char *buf, size_t bufsize)
+{
+    if (*bufpos - buf <= 0)
+        return 0;
+
+    (*bufpos)--;
+    **bufpos = '\0';
+    return 1;
+}
+
+void process_backspace(char **bufpos, char *buf, size_t bufsize)
+{
+    if (remove_char_from_buf(bufpos, buf, bufsize)) {
+        printf("\b \b");
+        fflush(stdout);
+    }
+}
+
 /* test: 1 line */
 int parse_input(FILE *out_f, FILE *dict_f)
 {
-    char read_buf[io_bufsize];
-    char out_f_buf[io_bufsize+1]; /* for \0 */
+    char read_buf[read_bufsize];
+    char out_f_buf[output_bufsize];
 
     int read_res;
     char *r_bufp, *f_bufp;
@@ -99,25 +130,39 @@ int parse_input(FILE *out_f, FILE *dict_f)
     while ((read_res = read(0, read_buf, sizeof(read_buf))) > 0) {
         for (r_bufp = read_buf; r_bufp-read_buf < read_res; r_bufp++) {
             switch (*r_bufp) {
+                case '\4':
+                    if (f_bufp == out_f_buf)
+                        goto end_of_file;
+                    break;
                 case '\n':
                     /* flush line and reset cur buf */
+                    fprintf(out_f, "%s\n", out_f_buf);
+                    f_bufp = out_f_buf;
+                    putchar_now('\n');
                     break;
                 case '\t':
                     /* look up current word prefix (check from out buf) */
                     break;
                 case '\b':
                     /* remove from out buf, "\b \b" to screen */
+                    process_backspace(&f_bufp, out_f_buf, sizeof(out_f_buf));
                     break;
                 case del:
                     /* same as \b */
+                    process_backspace(&f_bufp, out_f_buf, sizeof(out_f_buf));
                     break;
                 default:
                     /* put to out buf and to screen */
+                    if (add_char_to_buf(&f_bufp, out_f_buf, 
+                                sizeof(out_f_buf), *r_bufp)) {
+                        putchar_now(*r_bufp);
+                    }
                     break;
             }
         }
     }
 
+end_of_file:
     return 0;
 }
 
@@ -151,11 +196,13 @@ int main(int argc, char **argv)
 
     tcgetattr(0, &ts1); 
     memcpy(&ts2, &ts1, sizeof(ts1)); 
+
     ts1.c_lflag &= ~(ICANON | IEXTEN | ECHO);
     ts1.c_lflag |= ISIG;
     ts1.c_cc[VMIN] = 1;
     ts1.c_cc[VTIME] = 0;
-    ts1.c_cc[VEOF] = 0;
+    ts1.c_cc[VEOF] = 0; /* CTRL_D seems not to work */
+
     tcsetattr(0, TCSANOW, &ts1); 
 
     exit_code = parse_input(out_f, dict_f);
