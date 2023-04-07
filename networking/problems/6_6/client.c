1fa67d7a25f5906f5fa5d9c35647f34085c983bb
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-04-07 19:58:35 +0300

    Completed 6_6 (locally tested)

--- networking/problems/6_6/client.c
+++ networking/problems/6_6/client.c
@@ -1,5 +1,8 @@
 /* 6_6/client.c */
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,5 +16,50 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    struct sockaddr_in serv_addr;
+    unsigned short serv_port;
+    serv_addr.sin_family = AF_INET;
+
+    int ok = inet_aton(argv[1], &serv_addr.sin_addr);
+    if (!ok) {
+        fprintf(stderr, "Provide valid server ip-address\n");
+        return EXIT_FAILURE;
+    }
+
+    int scan_res = sscanf(argv[2], "%hu", &serv_port);
+    if (scan_res != 1) {
+        fprintf(stderr, "Provide valid server port\n");
+        return EXIT_FAILURE;
+    }
+    serv_addr.sin_port = htons(serv_port);
+
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_fd == -1) {
+        fprintf(stderr, "Failed to open socket\n");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(0);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    int res = bind(sock_fd, (struct sockaddr *) &addr, sizeof(addr));
+    if (res == -1) {
+        fprintf(stderr, "Failed to bind to socket\n");
+        close(sock_fd);
+        return EXIT_FAILURE;
+    }
+
+    size_t dgram_len = strlen(argv[3]);
+    res = sendto(sock_fd, argv[3], dgram_len, 0,
+            (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if (res != dgram_len) {
+        fprintf(stderr, "Failed to send full dgram\n");
+        close(sock_fd);
+        return EXIT_FAILURE;
+    }
+
+    close(sock_fd);
     return 0;
 }
--- networking/problems/6_6/server.c
+++ networking/problems/6_6/server.c
@@ -1,18 +1,81 @@
 /* 6_6/server.c */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+int sock_fd = -1;
+
+void int_handler(int s) {
+    if (sock_fd != -1)
+        close(sock_fd);
+    printf("Connection closed\n");
+    exit(EXIT_SUCCESS);
+}
+
+int char_is_intelligible(char c) {
+    return c == '\n' || c == '\t' || (c >= ' ' && c <= '~');
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         fprintf(stderr, "Specify args: port\n");
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    // The thing
+    signal(SIGINT, int_handler);
+    signal(SIGTERM, int_handler);
 
-    return 0;
+    unsigned short port;
+    int scan_res = sscanf(argv[1], "%hu", &port);
+    if (scan_res != 1) {
+        fprintf(stderr, "Provide valid port\n");
+        return EXIT_FAILURE;
+    }
+
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_fd == -1) {
+        fprintf(stderr, "Failed to open socket\n");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    int res = bind(sock_fd, (struct sockaddr *) &addr, sizeof(addr));
+    if (res == -1) {
+        fprintf(stderr, "Failed to bind to socket\n");
+        close(sock_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in from;
+    socklen_t fromlen;
+    char buf[64];
+    int bytes_recieved;
+
+    while ((bytes_recieved = recvfrom(sock_fd, &buf, sizeof(buf), 0,
+                    (struct sockaddr *) &from, &fromlen)) != -1) {
+        printf("Client ip-address: %s, port: %hu\n",
+                inet_ntoa(from.sin_addr), from.sin_port);
+
+        for (char *bp = buf; bp-buf < bytes_recieved; bp++) {
+            if (char_is_intelligible(*bp))
+                putchar(*bp);
+            else
+                putchar('?');
+        }
+
+        putchar('\n');
+    }
+
+    fprintf(stderr, "Error while waiting for dgram\n");
+    close(sock_fd);
+    return EXIT_FAILURE;
 }
