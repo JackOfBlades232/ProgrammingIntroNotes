37eef647367a6dae78af60aca22ccfa28fa92de2
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-04-08 10:33:27 +0300

    COmpleted 6_8

--- networking/problems/6_8.c
+++ networking/problems/6_8.c
@@ -10,7 +10,7 @@
 #include <arpa/inet.h>
 
 enum {
-    port = 7000
+    port = 7654
 };
 
 int sock_fd = -1;
@@ -26,15 +26,18 @@ int main() {
     signal(SIGINT, int_handler);
     signal(SIGTERM, int_handler);
     
-    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd == -1) {
         fprintf(stderr, "Failed to open socket\n");
         return EXIT_FAILURE;
     }
 
+    int opt = 1;
+    setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(0);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(sock_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
@@ -57,12 +60,17 @@ int main() {
                     (struct sockaddr *) &client_addr, &client_len)) != -1) {
         time_t t = time(NULL);
         size_t reply_len = sprintf(
-            reply_buf, "%s:\nip-address: %s, port: %hu\n", ctime(&t),
+            reply_buf, "%sip-address: %s, port: %hu\n", ctime(&t),
             inet_ntoa(client_addr.sin_addr), client_addr.sin_port
         );
         int res = send(connection_sock, reply_buf, reply_len, 0);
 
-        if (res != reply_len) {
+        close(connection_sock);
+        if (res != reply_len)
+            break;
+    }
 
-        }
+    fprintf(stderr, "Failed to serve client\n");
+    close(sock_fd);
+    return EXIT_FAILURE;
 }
