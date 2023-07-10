fcc0c3e237c6f886b08192bb885bc25a206be73b
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-07-10 15:45:03 +0300

    Added main notes to 7.3 (pthreads)

---
+++ parallel/notes/demo.c
@@ -0,0 +1,114 @@
+/* parallel/demo.c */
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+// This program consists of an integer array initialized to all zeroes,
+// and a number of threads that constantly choose two elements of the array,
+// decrement one and increment the other. The goal is to preserve the
+// array sum being equal to 0
+
+#define ARRAY_SIZE 10
+#define WORKERS_COUNT 3     // num threads
+#define PAUSE_LENGTH 1      // main thread pause between array sum check
+
+#ifndef PROTECTION
+#define PROTECTION 1        // 1 means use mutexes and semaphores, 0 -- no
+#endif
+
+// Init data to pass to a thread (only one copy, data_sem to control it's use)
+struct thread_start_data {
+    unsigned int randseed;
+    int *array;
+#if PROTECTION == 1
+    pthread_mutex_t *arr_mutex;
+    sem_t *data_sem;
+#endif
+};
+
+static void *worker_thread(void *v_data)
+{
+    struct thread_start_data *data = v_data;
+    unsigned int randseed = data->randseed;
+    int *array = data->array;
+#if PROTECTION == 1
+    pthread_mutex_t *arr_mutex = data->arr_mutex;
+    sem_t *data_sem = data->data_sem;
+    sem_post(data_sem);     // The data struct is not in use any more
+#endif
+
+    for (;;) {
+        int idx1, idx2;
+        idx1 = rand_r(&randseed) % ARRAY_SIZE;
+        idx2 = rand_r(&randseed) % ARRAY_SIZE;
+        if (idx1 == idx2)
+            continue;
+
+#if PROTECTION == 1
+        pthread_mutex_lock(arr_mutex);
+#endif
+        array[idx1]++;
+        array[idx2]--;
+#if PROTECTION == 1
+        pthread_mutex_unlock(arr_mutex);
+#endif
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    int i;
+    int array[ARRAY_SIZE];
+    struct thread_start_data tsdata;
+    pthread_t thr;
+#if PROTECTION == 1
+    pthread_mutex_t arr_mutex = PTHREAD_MUTEX_INITIALIZER;
+    sem_t tsd_sem;
+#endif
+
+    for (i = 0; i < ARRAY_SIZE; i++)
+        array[i] = 0;
+
+    tsdata.randseed = time(NULL);
+    tsdata.array = array;
+#if PROTECTION == 1
+    tsdata.arr_mutex = &arr_mutex;
+    tsdata.data_sem = &tsd_sem;
+    sem_init(&tsd_sem, 0, 0);
+#endif
+
+    // Init all workers with defferent seeds
+    for (i = 0; i < WORKERS_COUNT; i++) {
+        tsdata.randseed++;
+        pthread_create(&thr, NULL, worker_thread, &tsdata);
+#if PROTECTION == 1
+        sem_wait(&tsd_sem);
+#endif
+    }
+    
+    for (;;) {
+        int sum;
+#if PROTECTION == 1
+        pthread_mutex_lock(&arr_mutex);
+#endif
+        sum = 0;
+        for (i = 0; i < ARRAY_SIZE; i++)
+            sum += array[i];
+        printf("%d ", sum);
+        for (i = 0; i < ARRAY_SIZE; i++)
+            printf("%c%d", i ? ',' : '(', array[i]);
+        printf(")\n");
+#if PROTECTION == 1
+        pthread_mutex_unlock(&arr_mutex);
+#endif
+
+        sleep(PAUSE_LENGTH);
+    }
+
+    return 0;
+}
---
+++ parallel/notes/pthread.c
@@ -0,0 +1,111 @@
+/* parallel/pthread.c */
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+
+static int global_counter = 0;
+
+// Mutex is some internal lib structure, to be inited with this macro
+static pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+// Semaphore is the same, but has to be inited in a function and
+// can serve processes with shared mem, not only threads
+static sem_t talking_stick_semaphore;
+
+// Each thread has a main function, which takes anything as param
+// and returns anything
+void *thread_main(void *arg)
+{
+    pthread_mutex_lock(&counter_mutex); // Blocking version
+    /* pthread_mutex_trylock(counter_mutex); */ // Nonblocking version
+    global_counter++;
+    pthread_mutex_unlock(&counter_mutex);
+
+    sem_wait(&talking_stick_semaphore); // down
+    /* sem_trywait(&talking_stick_semaphore); */ // Nonblocking version
+
+    int sem_val;
+    sem_getvalue(&talking_stick_semaphore, &sem_val);
+
+    printf("Thread signing off, counter is %d, talking stick charges left %d\n",
+            global_counter, sem_val);
+
+    sem_post(&talking_stick_semaphore); // up
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+void *master_thread_main(void *arg)
+{
+    // detach makes the thread be freed completely on termination,
+    // so as not to become a zombie
+    pthread_detach(pthread_self());
+
+    // can work with processes, so is not prefixed with pthread
+    int sem_init_res = sem_init(&talking_stick_semaphore, 0, 4);
+    // All semaphore funcs return 0/-1 and set errno
+    if (sem_init_res == -1) {
+        perror("sem_init");
+        pthread_exit(NULL);
+    }
+
+    int num_threads = *((int *) arg);
+    pthread_t *threads = malloc(num_threads * sizeof(*threads));
+    int *thread_indicators = malloc(num_threads * sizeof(*thread_indicators));
+    for (int i = 0; i < num_threads; i++) {
+        // Takes pointer to thread, params, main thread func and void * input
+        int cr_res = pthread_create(threads+i, NULL, thread_main, NULL);
+        if (cr_res == EAGAIN) { // Only this code here
+            perror("pthread_create");
+            thread_indicators[i] = 0;
+            continue;
+        }
+
+        thread_indicators[i] = 1;
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        if (thread_indicators[i]) {
+            // Can mark other thread for cancellation, will be cancelled
+            // on cancellation points (like pthread_ calls)
+            if (i % 2 != 0)
+                pthread_cancel(threads[i]);
+
+            // Analogous to waitpid, can also pass pointer to void * to collect res
+            pthread_join(threads[i], NULL);
+        }
+    }
+
+    // Destroys all data if mutex is open, otherwise fail
+    // On linux mutex has no allocated data, but still this is good as a
+    // scope check
+    int md_res = pthread_mutex_destroy(&counter_mutex);
+    if (md_res == EAGAIN)
+        fprintf(stderr, "Mutex still in use!!\n");
+
+    // Same with semaphores, but!! It does not check shit, just leads to
+    // undef behaviour, so we must check ourselves
+    sem_destroy(&talking_stick_semaphore);
+    
+    return NULL;
+}
+
+int main()
+{
+    int num_threads = 32;
+    pthread_t master_thread;
+
+    int res = pthread_create(&master_thread, NULL, master_thread_main, &num_threads);
+    if (res == EAGAIN) { // Does not access errno, it seems
+        perror("pthread_create");
+        return 1;
+    }
+
+    for (;;) {}
+
+    return 0;
+}
