4a689a61c1c8bd605d673cf80e8a036acefc26c1
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-08-21 20:48:47 +0300

    Non-rec pattern matching done

--- paradigms/problems/9_7.c
+++ paradigms/problems/9_7.c
@@ -7,57 +7,83 @@
  * Non recursive naive wildcard pattern matching
  */
 
-#define NOOP
+void remove_consecutive_stars(char *str)
+{
+    int len = strlen(str);
+
+    for (int i = 0; i < len-1; i++) {
+        int offset = 0;
+        while (str[i+offset] == '*' && str[i+offset+1] == '*')
+            offset++;
+        for (int j = 0; j < len - i - offset + 1; j++)
+            str[i+j] = str[i+j+offset];
+        len -= offset;
+    }
+}
 
 struct star_frame {
     int str_idx, pat_idx;
 };
 
-int match(const char *str, const char *pat)
+int match(const char *str, const char *ptn)
 {
+    char *pat = strdup(ptn);
+    remove_consecutive_stars(pat);
+
     int str_len = strlen(str);
     int pat_len = strlen(pat);
 
-    struct star_frame *star_stack = malloc(str_len * sizeof(*star_stack));
-
-    for (int i = 0; i <= str_len-pat_len; i++) {
-        int s_idx = i;
-        int p_idx = 0;
-
-        int stack_head = -1;
-
-        while (s_idx < str_len && p_idx < pat_len) {
-            if (pat[p_idx] == '*') {
-                p_idx++;
-                stack_head++;
-                star_stack[stack_head].str_idx = s_idx;
-                star_stack[stack_head].pat_idx = p_idx;
-            } else if (str[s_idx] != pat[p_idx] && pat[p_idx] != '?') {
-                while (str_len - s_idx < pat_len - p_idx) {
-                    if (stack_head < 0)
-                        goto inner_loop_break;
-
-                    star_stack[stack_head].str_idx++;
-                    s_idx = star_stack[stack_head].str_idx;
-                    p_idx = star_stack[stack_head].pat_idx;
-                    stack_head--;
-                }
-
-                stack_head++;
-            } else {
-                s_idx++;
-                p_idx++;
-            }
-        }
-
-inner_loop_break:
-        NOOP;
-
-        if (p_idx == pat_len)
-            return 1;
+    int stars_left = 0;
+    for (int i = 0; i < pat_len; i++) {
+        if (pat[i] == '*')
+            stars_left++;
     }
 
-    return 0;
+    struct star_frame *star_stack = malloc(stars_left * sizeof(*star_stack));
+    int stack_head = -1;
+
+    int s_idx = 0,
+        p_idx = 0;
+
+    for (;;) {
+        if (p_idx < pat_len && pat[p_idx] == '*') {
+            p_idx++;
+            stack_head++;
+            stars_left--;
+            star_stack[stack_head].str_idx = s_idx;
+            star_stack[stack_head].pat_idx = p_idx;
+        } else if (s_idx >= str_len)
+            break;
+        else if (
+                p_idx >= pat_len || 
+                (str[s_idx] != pat[p_idx] && pat[p_idx] != '?')
+                )
+        {
+            if (stack_head < 0)
+                break;
+            star_stack[stack_head].str_idx++;
+            s_idx = star_stack[stack_head].str_idx;
+            p_idx = star_stack[stack_head].pat_idx;
+
+            while (str_len - s_idx < pat_len - p_idx - stars_left) {
+                stack_head--;
+                stars_left++;
+                if (stack_head < 0)
+                    goto loop_break;
+
+                star_stack[stack_head].str_idx++;
+                s_idx = star_stack[stack_head].str_idx;
+                p_idx = star_stack[stack_head].pat_idx;
+            }
+        } else {
+            s_idx++;
+            p_idx++;
+        }
+    }
+
+loop_break:
+    return p_idx >= pat_len &&
+        (s_idx >= str_len || (p_idx > 0 && pat[p_idx-1] == '*'));
 }
 
 void strip_nl(char *str)
