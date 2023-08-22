b5ecf1e433798ef50361e8bd24cdabe76c752736
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-08-22 11:06:34 +0300

    Implemented 9_11 (perm gen fsm), thus concluding chapter 9

--- paradigms/problems/9_11.c
+++ paradigms/problems/9_11.c
@@ -3,10 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+void swap_int(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int factorial(int n)
+{
+    int res = 1;
+    for (int i = 2; i <= n; i++) {
+        res *= i;
+        if (res < 0)
+            return -1;
+    }
+    return res;
+}
+
 struct perm_fsm {
     int n;
     int *last_perm;
     int longest_inv_tail;
+    int done;
 };
 
 struct perm_fsm *fsm_create(int n)
@@ -17,15 +36,52 @@ struct perm_fsm *fsm_create(int n)
     for (int i = 0; i < n; i++)
         fsm->last_perm[i] = i+1;
     fsm->longest_inv_tail = 1;
+    fsm->done = 0;
     return fsm;
 }
 
 /* won't bother with out buf safety now */
 void fsm_step(struct perm_fsm *fsm, int *out)
 {
-    memcpy(out, fsm->last_perm, fsm->n * sizeof(int));
+    memcpy(out, fsm->last_perm, fsm->n * sizeof(*fsm->last_perm));
 
-    /* TODO: step the permutation */
+    /* If already generated all permutations, just clear the outbuf */
+    if (fsm->done)
+        return;
+    else if (!fsm->done && fsm->longest_inv_tail >= fsm->n) {
+        memset(fsm->last_perm, 0, fsm->n * sizeof(*fsm->last_perm));
+        fsm->done = 1;
+        return;
+    }
+
+    /* find the first element larger than pivot in tail
+     * (the tail is the longest motonously decreasing suffix) */
+    int pivot_idx = fsm->n - fsm->longest_inv_tail - 1;
+    int next_idx;
+    for (next_idx = fsm->n - 1; next_idx > pivot_idx; next_idx--) {
+        if (fsm->last_perm[next_idx] > fsm->last_perm[pivot_idx])
+            break;
+    }
+
+    /* swap it with the pivot */
+    swap_int(&fsm->last_perm[pivot_idx], &fsm->last_perm[next_idx]);
+
+    /* reverse the tail */
+    for (int i = 0; i < fsm->longest_inv_tail / 2; i++)
+        swap_int(&fsm->last_perm[pivot_idx+i+1], &fsm->last_perm[fsm->n-i-1]);
+
+    /* reset the tail len: if tail was longer that 1 elem, after the reverse
+     * it becomes 1 elem long, otherwise we have to recalculate it's len */
+    if (fsm->longest_inv_tail > 1)
+        fsm->longest_inv_tail = 1;
+    else {
+        for (int i = fsm->n-1; i >= 1; i--) {
+            if (fsm->last_perm[i-1] <= fsm->last_perm[i])
+                break;
+            fsm->longest_inv_tail++;
+        }
+    }
+
 }
 
 void fsm_free(struct perm_fsm *fsm) 
@@ -46,7 +102,13 @@ int main()
     struct perm_fsm *fsm = fsm_create(n);
     int *outbuf = malloc(n * sizeof(*outbuf));
 
-    for (int i = 0; i < n; i++) {
+    int nfac = factorial(n);
+    if (nfac == -1) {
+        fprintf(stderr, "N is too large!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < nfac; i++) {
         fsm_step(fsm, outbuf);
 
         putchar('{');
