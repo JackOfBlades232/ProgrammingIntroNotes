bc81ca4f9577c3872be65618c465e021bb3b9c43
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-08-25 14:54:53 +0300

    Completed 10_8

--- cpp/problems/10_8.cpp
+++ cpp/problems/10_8.cpp
@@ -1,30 +1,41 @@
 /* 10_8.cpp */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef long long ll;
-typedef unsigned long long ull;
+
+#define ABS(_a) ((_a) >= 0 ? (_a) : -(_a))
+#define SGN(_a) ((_a) > 0 ? 1 : ((_a) < 0 ? -1 : 0))
+
+// Just a container used for it's destructor
+class StringCont {
+    char *str;
+
+public:
+    StringCont(char *a_str) : str(a_str) {}
+    ~StringCont() { free(str); }
+    const char *Str() const { return str; }
+};
 
 class Rational {
     ll numerator;
-    ull denominator;
-
-    static char string_repr[1024];
+    ll denominator;
 
 public:
-    // Should not allow denominator=0
-    Rational(ll a_numerator = 0, ull a_denominator = 1)
-        : numerator(a_numerator), denominator(a_denominator) {}
+    // Should not allow denominator <= 0
+    Rational(ll a_numerator = 0, ll a_denominator = 1)
+        : numerator(a_numerator), denominator(a_denominator) 
+        { if (denominator <= 0) { fprintf(stderr, "Denom<=0\n"); exit(1); } }
 
-    ll Numerator() { return numerator; }
-    ull Denominator() { return denominator; }
+    ll Numerator() const { return numerator; }
+    ll Denominator() const { return denominator; }
 
-    operator double() { return (double)numerator/denominator; }
-    operator ll() { return (double)(*this); }
+    operator double() const { return (double)numerator/denominator; }
+    operator ll() const { return numerator/((ll)denominator); } 
+    ll Round() const;
 
-    ll floor() { return numerator/denominator; } 
-
-    // Not to be assigned, for use in printf (once per call)
-    const char *AsString();
+    StringCont ToString() const;
 
     const Rational &operator+=(const Rational &op);
     const Rational &operator-=(const Rational &op);
@@ -37,25 +48,31 @@ public:
     friend Rational operator/(const Rational &r1, const Rational &r2);
 };
 
-char Rational::string_repr[1024] = { 0 };
-
-const char *Rational::AsString()
+ll Rational::Round() const
 {
-    snprintf(string_repr, sizeof(string_repr), 
-             "%lld/%lld", numerator, denominator);
-    return string_repr;
+    ll whole = (ll)(*this);
+    double fr = (double)(*this) - whole; 
+    return whole + (ABS(fr) >= 0.5 ? SGN(fr) : 0);
+}
+
+StringCont Rational::ToString() const
+{
+    size_t nchars = snprintf(NULL, 0, "%lld/%lld", numerator, denominator);
+    char *str = (char *)malloc((nchars+1) * sizeof(*str));
+    sprintf(str, "%lld/%lld", numerator, denominator);
+    return StringCont(str);
 }
 
 const Rational &Rational::operator+=(const Rational &op)
 {
-    numerator = numerator*op.denominator + denominator*op.numerator;
+    numerator = numerator*op.denominator + op.numerator*denominator;
     denominator = denominator*op.denominator;
     return *this;
 }
 
 const Rational &Rational::operator-=(const Rational &op)
 {
-    numerator = numerator*op.denominator - denominator*op.numerator;
+    numerator = numerator*op.denominator - op.numerator*denominator;
     denominator = denominator*op.denominator;
     return *this;
 }
@@ -70,7 +87,7 @@ const Rational &Rational::operator*=(const Rational &op)
 const Rational &Rational::operator/=(const Rational &op)
 {
     numerator = numerator*op.denominator;
-    denominator = denominator*op.numerator;
+    denominator = op.numerator*denominator;
     return *this;
 }
 
@@ -94,8 +111,13 @@ Rational operator*(const Rational &r1, const Rational &r2)
 
 Rational operator/(const Rational &r1, const Rational &r2)
 {
-    return Rational(r1.numerator * r2.denominator,
-                    r1.denominator * r2.numerator);
+    ll numerator = r1.numerator * r2.denominator;
+    ll denominator = r2.numerator * r1.denominator;
+    if (denominator < 0) {
+        numerator *= -1;
+        denominator *= -1;
+    }
+    return Rational(numerator, denominator);
 }
 
 int main()
@@ -104,15 +126,96 @@ int main()
     Rational r2(-32);
     Rational r3(15, 6);
     Rational r4(102.3421);
-    printf("r1() = %s\n", r1.AsString());
-    printf("r2(-32) = %s\n", r2.AsString());
-    printf("r3(15, 6) = %s\n", r3.AsString());
-    printf("r4(102.3421) = %s\n", r4.AsString());
+
+    printf("r1() = %s\n", r1.ToString().Str());
+    printf("r2(-32) = %s\n", r2.ToString().Str());
+    printf("r3(15, 6) = %s\n", r3.ToString().Str());
+    printf("r4(102.3421) = %s\n", r4.ToString().Str());
+
+    putchar('\n');
 
     printf("r3.Numerator() = %lld, r3.Denominator() = %lld\n",
            r3.Numerator(), r3.Denominator());
+ 
+    putchar('\n');
 
-    // @TODO: full demostration-test coverage
+    printf("(double)r1 = %lf\n", (double)r1);
+    printf("(double)r2 = %lf\n", (double)r2);
+    printf("(double)r3 = %lf\n", (double)r3);
+
+    Rational r5 = Rational(-9, 31);
+    printf("(double)(%s) = %lf\n", r5.ToString().Str(), (double)r5);
+
+    putchar('\n');
+
+    Rational r6 = Rational(17, 6);
+    printf("(long long)r1 = %lld\n", (ll)r1);
+    printf("(long long)r2 = %lld\n", (ll)r2);
+    printf("(long long)r3 = %lld\n", (ll)r3);
+    printf("(long long)(%s) = %lld\n", r5.ToString().Str(), (ll)r5);
+    printf("(long long)(%s) = %lld\n", r6.ToString().Str(), (ll)r6);
+
+    putchar('\n');
+
+    printf("round(r3) = %lld\n", r3.Round());
+    printf("round(%s) = %lld\n", r5.ToString().Str(), r5.Round());
+    printf("round(%s) = %lld\n", r6.ToString().Str(), r6.Round());
+
+    putchar('\n');
+
+    printf("%s + %s = %s\n", r5.ToString().Str(), r6.ToString().Str(), 
+           (r5+r6).ToString().Str());
+    printf("%s + %s = %s\n", 
+           Rational(22, 10).ToString().Str(), Rational(-13, 8).ToString().Str(),
+           (Rational(22, 10) + Rational(-13, 8)).ToString().Str());
+    printf("%s + %s = %s\n", 
+           Rational(-1, 3).ToString().Str(), Rational(1, 27).ToString().Str(),
+           (Rational(-1, 3) + Rational(1, 27)).ToString().Str());
+
+    putchar('\n');
+
+    printf("%s - %s = %s\n", 
+           r5.ToString().Str(), Rational(2, 5).ToString().Str(), 
+           (r5 - Rational(2, 5)).ToString().Str());
+    printf("%s - %s = %s\n", 
+           Rational(0, 7).ToString().Str(), Rational(-33, 412).ToString().Str(),
+           (Rational(0, 7) - Rational(-33, 412)).ToString().Str());
+    printf("%s - %s = %s\n", 
+           Rational(2, 2).ToString().Str(), Rational(33, 33).ToString().Str(),
+           (Rational(2, 2) - Rational(33, 33)).ToString().Str());
+
+    putchar('\n');
+
+    printf("%s * %s = %s\n", 
+           r5.ToString().Str(), Rational(2, 5).ToString().Str(),
+           (r5 * Rational(2, 5)).ToString().Str());
+    printf("%s * %s = %s\n", 
+           Rational(0, 7).ToString().Str(), Rational(-33, 412).ToString().Str(),
+           (Rational(0, 7) * Rational(-33, 412)).ToString().Str());
+    printf("%s * %s = %s\n", 
+           Rational(-1, 3).ToString().Str(), Rational(1, 27).ToString().Str(),
+           (Rational(-1, 3) * Rational(1, 27)).ToString().Str());
+
+    putchar('\n');
+
+    printf("%s / %s = %s\n", r5.ToString().Str(), r6.ToString().Str(), 
+           (r5/r6).ToString().Str());
+    printf("%s / %s = %s\n", 
+           Rational(22, 10).ToString().Str(), Rational(-13, 8).ToString().Str(),
+           (Rational(22, 10) / Rational(-13, 8)).ToString().Str());
+    printf("%s / %s = %s\n", 
+           Rational(2, 2).ToString().Str(), Rational(33, 33).ToString().Str(),
+           (Rational(2, 2) / Rational(33, 33)).ToString().Str());
+
+    Rational r(0);
+    printf("r = %s\n", r.ToString().Str());
+    printf("r += %s = %s\n", Rational(13, 46).ToString().Str(),
+           (r += Rational(13, 46)).ToString().Str());
+    printf("r -= %s = %s\n", Rational(122, 55).ToString().Str(),
+           (r -= Rational(122, 55)).ToString().Str());
+    printf("r *= %s = %s\n", Rational(8, 3).ToString().Str(),
+           (r *= Rational(8, 3)).ToString().Str());
+    printf("r /= 7 = %s\n", (r /= 7).ToString().Str());
 
     return 0;
 }
