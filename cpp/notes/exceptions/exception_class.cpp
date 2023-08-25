848bcfac82698d9d81b08a4d1942da053b7b196e
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-08-25 17:34:27 +0300

    Added notes to 10.5 (exception handling)

---
+++ cpp/notes/exceptions/exception_class.cpp
@@ -0,0 +1,88 @@
+/* exceptions/throw_catch.cpp */
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+// If we want to pass some specific data down to the handler, and/or identify
+// our exception somehow, it is wise to use an exception class/struct
+// It is not special, but it has to have a copy constructor and a destructor,
+// cause it will have to be moving from one stack frame to another, if it is
+// local (and why would it not be?)
+class FileException {
+    int err_code;
+    char *filename;
+    char *comment;
+
+public:
+    FileException(const char *fn, const char *cmt);
+    FileException(const FileException &other);
+    ~FileException();
+
+    const char *GetName() const { return filename; }
+    const char *GetComment() const { return comment; }
+    int GetErrno() const { return err_code; }
+
+private:
+    static char *strdup(const char *str);
+};
+
+FileException::FileException(const char *fn, const char *cmt)
+{
+    err_code = errno;
+    filename = strdup(fn);
+    comment = strdup(cmt);
+}
+
+FileException::FileException(const FileException &other)
+{
+    err_code = other.err_code;
+    filename = strdup(other.filename);
+    comment = strdup(other.comment);
+}
+
+FileException::~FileException()
+{
+    delete[] filename;
+    delete[] comment;
+}
+
+char *FileException::strdup(const char *str)
+{
+    char *res = new char[strlen(str)+1];
+    strcpy(res, str);
+    return res;
+}
+
+unsigned int line_count_in_file(const char *filename)
+{
+    FILE *f = fopen(filename, "r");
+    if (!f)
+        throw FileException(filename, "couldn't open for line counting");
+    int n = 0;
+    int c = 0;
+    while ((c = fgetc(f)) != EOF) {
+        if (c == '\n')
+            n++;
+    }
+    fclose(f);
+    return n;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Args: <filename>\n");
+        return 1;
+    }
+    try {
+        printf("Lines in file: %d\n", line_count_in_file(argv[1]));
+    } 
+    catch(const FileException &ex) { 
+        // Here, both possible casts are used: FE -> FE & -> const FE &
+        fprintf(stderr, "File exception: %s (%s): %s\n", 
+                ex.GetName(), ex.GetComment(), strerror(ex.GetErrno()));
+        return 2;
+    } 
+
+    return 0;
+}
---
+++ cpp/notes/exceptions/throw_catch.cpp
@@ -0,0 +1,88 @@
+/* exceptions/throw_catch.cpp */
+#include <stdio.h>
+
+struct somestruct { int val = 666; const char *msg = "aaaaa"; };
+
+unsigned int line_count_in_file(const char *file_name)
+{
+    FILE *f = fopen(file_name, "r");
+    // Throwed exception can be any type with a copy-constr and a destructor,
+    // and it falls through the call stack until caught. There exists a 
+    // style of programming (even with return codes), when all exceptional
+    // situations are dealt with in the main function
+    if (!f)
+        throw "couldn't open the file";
+    int n = 0;
+    int c = 0;
+    while ((c = fgetc(f)) != EOF) {
+        if (c == '\n')
+            n++;
+    }
+    fclose(f);
+
+    if (n % 6 == 0)
+        throw 666;
+    else if (n % 3 == 0)
+        throw somestruct();
+    return n;
+}
+
+// use of catch(...): free resources and throw further down
+void f(int n)
+{
+    int *p = new int[n];
+    try {
+        // Do smth
+    }
+    catch(...) {
+        delete [] p;
+        throw;
+    }
+
+    // there is good news though: when throwing all falling through,
+    // all local objects' destructors get called, aka automatic cleaning
+
+    delete [] p;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Args: <filename>\n");
+        return 1;
+    }
+
+    // If I never catch, it is a crash
+    // Catching works for funcs in try block, and if caught, goes to
+    // catch block with exact type (there can be may catch blocks for 
+    // different types)
+    // If an exception is not caught by any catch block, it continues
+    // it's descent down the call stack
+    try {
+        printf("Lines in file: %d\n", line_count_in_file(argv[1]));
+    } 
+    catch(const char *ex) {
+        fprintf(stderr, "Exception (string): %s\n", ex);
+        return 2;
+    } 
+    catch(int n) {
+        fprintf(stderr, "Exception (int): %d\n", n);
+        return 3;
+
+        throw; // this throws the last caught exception further down
+    } 
+    catch(...) { // catches any (other) type
+        fprintf(stderr, "There's something wrong! I should've known\n");
+        return 4;
+
+    }
+
+    // The types can be cast when catching, but only in a few ways
+    // (normal casting will not work):
+    // T -> T &; T -> const T (not the other way round); 
+    // and some stuff with inheritance
+
+    f(123);
+
+    return 0;
+}
