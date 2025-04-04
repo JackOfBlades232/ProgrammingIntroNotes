90f4de47865a62f6a3407a564a6085ec1cc7000d
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-04-04 21:26:33 +0300

    Rewrote some of the simplest tasks to scheme

--- nondestructive/problems/11_1-9.scm
+++ nondestructive/problems/11_1-9.scm
@@ -26,3 +26,56 @@
 )
 
 (output "11_1" (range 17))
+
+; 11_2
+(define (take-first n . expr)
+    (define (impl n expr)
+        (cond
+            ((not (integer? n)) 'WrongInput)
+            ((<= n 0) '())
+            ((null? expr) '())
+            (#t (cons (car expr) (impl (- n 1) (cdr expr))))
+        )
+    )
+    (impl n expr)
+)
+
+(output "11_2" (take-first  8 '(1 2 3) "asd" + '(aaa) '() #t '() '() '() '() '()))
+
+; 11_3 -- 11_7 won't redo, it's literally the same
+
+; 11_8
+(define (list-iter lst)
+    (define state lst)
+    (if (list? lst)
+        (lambda ()
+            (if (null? state)
+                '()
+                (let ((cur (car state)))
+                    (set! state (cdr state))
+                    cur
+                )
+            )
+        )
+        'WrongInput
+    )
+)
+
+(define li (list-iter '(1 2 "a")))
+(output "11_8" (li))
+(output "11_8" (li))
+(output "11_8" (li))
+(output "11_8" (li))
+(output "11_8" (li))
+(output "11_8" (list-iter 1))
+
+; 11_9
+(define (rev-list lst)
+    (define ret '())
+    (map (lambda (elem) (set! ret (cons elem ret))) lst)
+    ret
+)
+
+(output "11_9" (rev-list '(1 2 3 4 5 6 7 8 (9 9))))
+
+; 11_10 is also not getting rewritten in scheme
