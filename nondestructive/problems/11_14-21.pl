305d62891602da12e3eec494a9970d280d64ca4c
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-05-04 13:25:40 +0300

    Added solutions to 11_15 and 11_16

--- nondestructive/problems/11_14-21.pl
+++ nondestructive/problems/11_14-21.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/swipl -q
 
+% 11_14
+
 % A
 even_len_list([]).
 even_len_list([_]) :- fail.
@@ -18,3 +20,23 @@ two_repeated_list([_|Tail]) :- two_repeated_list(Tail).
 empty_lists_at_evens_list([_, []|Tail]) :- empty_lists_at_evens_list(Tail), !.
 empty_lists_at_evens_list([_]).
 empty_lists_at_evens_list([]).
+
+% 11_15
+
+none(_, []).
+none(E, [X|Tail]) :- E \= X, none(E, Tail).
+
+one(E, [E|Tail]) :- !, none(E, Tail).
+one(E, [_|Tail]) :- one(E, Tail).
+
+atleast2(E, [E|Tail]) :- one(E, Tail).
+atleast2(E, [_|Tail]) :- atleast2(E, Tail).
+
+% 11_16
+
+lst([]).
+lst([_|Tail]) :- lst(Tail).
+
+similar([], []).
+similar([F|FTail], [[]|ETail]) :- lst(F), similar(FTail, ETail).
+similar([F|FTail], [F|ETail]) :- similar(FTail, ETail).
--- nondestructive/problems/11_15.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/swipl -q
-
-% @TODO: still wrong, fix
-
-count_element(E, [E|_], N) :- !.
-count_element(E, [_|Tail]) :- has_element(E, Tail).
-
-has_two_of(E, [E|Tail]) :- has_element(E, Tail).
-has_two_of(E, [_|Tail]) :- has_two_of(E, Tail).
