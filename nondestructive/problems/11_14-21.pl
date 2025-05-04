098cdd0220b50d9059625a99a569ec21d541e931
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-05-04 16:30:16 +0300

    Added solutions to 11_17-11_21

--- nondestructive/problems/11_14-21.pl
+++ nondestructive/problems/11_14-21.pl
@@ -40,3 +40,57 @@ lst([_|Tail]) :- lst(Tail).
 similar([], []).
 similar([F|FTail], [[]|ETail]) :- lst(F), similar(FTail, ETail).
 similar([F|FTail], [F|ETail]) :- similar(FTail, ETail).
+
+% 11_17
+
+eq([], []).
+eq([X|FTail], [X|STail]) :- eq(FTail, STail).
+
+ins_one(List, Elem, [Elem|RTail]) :- eq(List, RTail).
+ins_one([X|LTail], Elem, [X|RTail]) :- ins_one(LTail, Elem, RTail).
+
+% 11_18
+
+% A
+shrink2any_helper([_|LTail], R, 0) :- shrink2any_helper(LTail, R, 1).
+shrink2any_helper([_|LTail], R, 1) :- eq(LTail, R).
+shrink2any_helper([X|LTail], [X|RTail], N) :-
+    shrink2any_helper(LTail, RTail, N).
+
+shrink2any(L, R) :- shrink2any_helper(L, R, 0).
+
+% B
+shrink2atmost_helper([_|LTail], R, 0) :-
+    eq(LTail, R);
+    shrink2any_helper(LTail, R, 1).
+shrink2atmost_helper([_|LTail], R, 1) :- eq(LTail, R).
+shrink2atmost_helper([X|LTail], [X|RTail], N) :-
+    shrink2atmost_helper(LTail, RTail, N).
+
+shrink2atmost(L, R) :- eq(L, R); shrink2atmost_helper(L, R, 0).
+
+% C
+shrink2consecutive([_, _|LTail], R) :- eq(LTail, R).
+shrink2consecutive([X|LTail], [X|RTail]) :- shrink2consecutive(LTail, RTail).
+
+% 11_19
+
+strike(L, [[]|L]).
+strike([_|LTail], [[]|RTail]) :- strike(LTail, [[]|RTail]).
+strike([X|LTail], [X|RTail]) :- strike(LTail, RTail).
+
+% 11_20
+
+permute([], []).
+permute(L, [X|Tail]) :- ins_one(Rem, X, L), permute(Rem, Tail).
+
+% 11_21
+
+listrev(L, R) :- listrev(L, R, []).
+listrev([], A, A) :- !.
+listrev([H|T], R, A) :- listrev(T, R, [H|A]).
+
+uniq(L, R) :- uniq(L, RR, []), listrev(R, RR).
+uniq([], R, R) :- !.
+uniq([H|T], R, A) :- none(H, A), !, uniq(T, R, [H|A]).
+uniq([_|T], R, A) :- uniq(T, R, A).
