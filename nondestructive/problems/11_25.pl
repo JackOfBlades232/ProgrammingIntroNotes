3a746db10d3474dc703d1e4ccf1a4a3ac763a846
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-05-30 13:09:44 +0300

    Done (for the main pass, could be improved a lot)

--- nondestructive/problems/11_25.pl
+++ nondestructive/problems/11_25.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/swipl -q
 
-% @TODO: finish
+% @TODO(opt): make it run in adequate time for more boards
+%             with analytical pre-solution up to 3x3 board
+% It is quite tedious to do in prolog, so I won't consider it required
+
+use_module(library(heaps)).
 
 :- initialization(main).
 
@@ -12,16 +16,8 @@ inversion_count([], Out) :- Out is 0.
 inversion_count([H|T], Out) :- inversion_count(T, OutWithout),
     inversions_with(H, T, OutWith), Out is OutWith + OutWithout.
 
-even_permutation(L) :- inversion_count(L, Count), Count mod 2 =:= 0.
-
-list_is_fifteen(L) :-
-    Elems =
-    [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        '10', '11', '12', '13', '14', '15', '@'
-    ],
-    sort(Elems, ElemsSorted), sort(L, LSorted),
-    ElemsSorted = LSorted.
+even_permutation(L, Add) :-
+    inversion_count(L, Count), (Count + Add) mod 2 =:= 0.
 
 code_list_to_board(
     [E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16],
@@ -51,17 +47,6 @@ board_at(B, RI, EI, Elem) :- board_row(B, RI, Row), row_at(Row, EI, Elem).
 board_set(B, OB, RI, EI, Elem) :- 
     board_row(B, RI, R), row_set(R, NR, EI, Elem), board_row_set(B, OB, RI, NR).
 
-rowcol_to_linear(RI, EI, LI) :- LI is (RI - 1) * 4 + EI.
-linear_to_rowcol(LI, RI, EI) :-
-    RI is (LI - 1) div 4 + 1, EI is (LI - 1) mod 4 + 1.
-
-board_at(B, LI, Elem) :-
-    linear_to_rowcol(LI, RI, EI), board_at(B, RI, EI, Elem).
-board_search(B, LI, Elem) :-
-    board_at(B, RI, EI, Elem), rowcol_to_linear(RI, EI, LI).
-
-% @TODO: set by linear index?
-
 apply_turn_to_board(Board, OutBoard, ERI, EEI, RI, EI) :-
     board_at(Board, RI, EI, Subs),
     board_set(Board, OTB, ERI, EEI, Subs),
@@ -76,7 +61,6 @@ turns_from(RI, EI, _, _, RMax, _, ORI, OEI) :-
 turns_from(RI, EI, _, _, _, EMax, ORI, OEI) :-
     EI < EMax, ORI is RI, OEI is EI + 1.
 
-% @TODO: extend to partial boards
 turns_from(Board, OutBoard, ORI, OEI) :-
     board_at(Board, RI, EI, 16),
     turns_from(RI, EI, 1, 1, 4, 4, ORI, OEI),
@@ -84,7 +68,7 @@ turns_from(Board, OutBoard, ORI, OEI) :-
 
 diff_board_score_at(From, To, RI, EI, S) :-
     board_at(From, RI, EI, F), board_at(To, RI, EI, T),
-    (F =\= T, T =\= 16, S = 1; S = 0).
+    (F =\= T, T =\= 16, !, S = 1; S = 0).
 
 diff_board_score(From, To, 4, 4, S) :- !,
     diff_board_score_at(From, To, 4, 4, S).
@@ -94,17 +78,57 @@ diff_board_score(From, To, RI, EI, S) :-
     diff_board_score(From, To, NRI, NEI, Rest), S is Rest + Addition.
 diff_board_score(From, To, S) :- diff_board_score(From, To, 1, 1, S).
 
-choose_next_node([], Min, _) :- Min = 2^64 - 1.
-choose_next_node([HT|OT], Min, MinRec) :-
-    HT = record(_, G, H), Score is G + H,
-    choose_next_node(OT, OMin, OMinRec),
-    (Score < Min,
-        Min = Score, MinRec = HT;
-        Min = OMin, MinRec = OMinRec
+heuristic(From, To, S) :- diff_board_score(From, To, S).
+score(From, To, Depth, S) :-
+    heuristic(From, To, H), S is Depth + H.
+
+expand(node(Board, Depth, Trail), Closed, Final, Children) :-
+    findall(
+        node(Child, CDepth, CScore, [CTurn|Trail]),
+        (
+            CDepth is Depth + 1,
+            turns_from(Board, Child, ORI, OEI),
+            not(member(Child, Closed)),
+            score(Child, Final, CDepth, CScore),
+            CTurn = turn(ORI, OEI)
+        ),
+        Children).
+
+insert_all_into_open([], Open, Out) :- Out = Open.
+insert_all_into_open([NH|NT], Open, Out) :-
+    NH = node(B, D, Score, T),
+    add_to_heap(Open, Score, node(B, D, T), SOut),
+    insert_all_into_open(NT, SOut, Out).
+
+search([node(Final, _, _, Soln)|_], _, Final, Soln) :- !.
+search(Open, Closed, Final, Soln) :-
+    get_from_heap(Open, _, H, T),
+    (
+        H = node(Final, _, Soln), !;
+
+        expand(H, Closed, Final, Children),
+        insert_all_into_open(Children, T, N),
+        search(N, [H|Closed], Final, Soln)
     ).
 
-solve_board(Final, OpenList, ClosedList) :-
-    
+solve(Init, Final, Soln) :-
+    score(Init, Final, 0, InitScore),
+    singleton_heap(InitOpen, InitScore, node(Init, 0, [])),
+    search(InitOpen, [], Final, Soln).
+
+print_soln([]) :- nl.
+print_soln([turn(X, Y)|T]) :-
+    write(X), write(','), write(Y), nl,
+    print_soln(T).
+
+list_is_fifteen(L) :-
+    Elems =
+    [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '10', '11', '12', '13', '14', '15', '@'
+    ],
+    sort(Elems, ElemsSorted), sort(L, LSorted),
+    ElemsSorted = LSorted.
 
 atom_to_code_fifteen('@', 16) :- !.
 atom_to_code_fifteen(Atom, Code) :- atom_number(Atom, Code).
@@ -132,8 +156,9 @@ list_to_board_fifteen(
     atom_to_code_fifteen(E16, C16).
 
 final_board_fifteen(InitBoard, FinalBoard) :-
+    board_at(InitBoard, RI, EI, 16), Add is (4 - RI) + (4 - EI),
     code_list_to_board(InitCodes, InitBoard),
-    (even_permutation(InitCodes), !,
+    (even_permutation(InitCodes, Add), !,
         FinalBoard = board(
             row(1, 2, 3, 4),
             row(5, 6, 7, 8),
@@ -146,12 +171,57 @@ final_board_fifteen(InitBoard, FinalBoard) :-
             row(13, 15, 14, 16))
     ).
 
-% @TEST, to be removed
-turns_from_test(B, O) :- turns_from(B, OB, ORI, OEI), O = tfout(OB, ORI, OEI).
-print_tfouts([]).
-print_tfouts([tfout(OB, ORI, OEI)|T]) :- 
-    write(ORI), write(' '), write(OEI), nl, write(OB), nl,
-    print_tfouts(T).
+list_is_thirteen(L) :-
+    Elems =
+    [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '10', '11', '12', '13', '0', '00', '@'
+    ],
+    sort(Elems, ElemsSorted), sort(L, LSorted),
+    ElemsSorted = LSorted.
+
+atom_to_code_thirteen('@', 16) :- !.
+atom_to_code_thirteen('00', 1) :- !.
+atom_to_code_thirteen(Atom, Code) :-
+    atom_number(Atom, BCode), Code is BCode + 2.
+
+list_to_board_thirteen(
+    [E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16],
+    board(row(C1, C2, C3, C4), row(C5, C6, C7, C8), row(C9, C10, C11, C12),
+        row(C13, C14, C15, C16))
+) :-
+    atom_to_code_thirteen(E1, C1),
+    atom_to_code_thirteen(E2, C2),
+    atom_to_code_thirteen(E3, C3),
+    atom_to_code_thirteen(E4, C4),
+    atom_to_code_thirteen(E5, C5),
+    atom_to_code_thirteen(E6, C6),
+    atom_to_code_thirteen(E7, C7),
+    atom_to_code_thirteen(E8, C8),
+    atom_to_code_thirteen(E9, C9),
+    atom_to_code_thirteen(E10, C10),
+    atom_to_code_thirteen(E11, C11),
+    atom_to_code_thirteen(E12, C12),
+    atom_to_code_thirteen(E13, C13),
+    atom_to_code_thirteen(E14, C14),
+    atom_to_code_thirteen(E15, C15),
+    atom_to_code_thirteen(E16, C16).
+
+final_board_thirteen(InitBoard, FinalBoard) :-
+    board_at(InitBoard, RI, EI, 16), Add is (4 - RI) + (4 - EI),
+    code_list_to_board(InitCodes, InitBoard),
+    (even_permutation(InitCodes, Add), !,
+        FinalBoard = board(
+            row(1, 2, 3, 4),
+            row(5, 6, 7, 8),
+            row(9, 10, 11, 12),
+            row(13, 14, 15, 16));
+        FinalBoard = board(
+            row(2, 1, 3, 4),
+            row(5, 6, 7, 8),
+            row(9, 10, 11, 12),
+            row(13, 14, 15, 16))
+    ).
 
 main :-
     on_signal(int, _, default),
@@ -161,33 +231,17 @@ main :-
     (
         list_is_fifteen(Argv), !,
             list_to_board_fifteen(Argv, Board),
-            write(Board), nl,
-            final_board_fifteen(Board, Final),
-            write('Dest:'), nl,
-            write(Final), nl,
-
-            board_row(Board, 2, R), write(R), nl,
-            row_at(R, 4, E), write(E), nl,
-
-            board_at(Board, 3, 2, EE), write(EE), nl,
-            board_at(Board, RI, EI, 12), write(RI), write(' '), write(EI), nl,
-
-            board_at(Board, 11, EEE), write(EEE), nl,
-            board_search(Board, LI, 7), write(LI), nl,
-
-            findall(
-                tfout(OB, ORI, OEI),
-                turns_from_test(Board, tfout(OB, ORI, OEI)),
-                Turns),
-            print_tfouts(Turns),
-
-            diff_board_score(Board, Final, Score),
-            write('Score: '), write(Score), nl;
+            final_board_fifteen(Board, Final);
+        list_is_thirteen(Argv), !,
+            list_to_board_thirteen(Argv, Board),
+            final_board_thirteen(Board, Final);
             
-        % @TODO: thirteen
-
         write('Wrong input: use a permutation of [1-15] and @ for "fifteen", '),
-        write('and [0-13], 00 and @ for "thirteen"\n')
+        write('and [0-13], 00 and @ for "thirteen"\n'),
+        halt
     ),
+
+    solve(Board, Final, Soln),
+    print_soln(Soln),
 
     halt.
